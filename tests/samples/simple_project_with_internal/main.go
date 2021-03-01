e11acbc1e24359647be3d3dfc29c0db0b0a3fe51
白云辉 <baiyh@weipaitang.com>
2021-03-01 16:47:42 +0800

    fix: 添加remove

--- tests/samples/simple_project_with_internal/main.go
+++ tests/samples/simple_project_with_internal/main.go
@@ -3,11 +3,12 @@ package main
 import (
 	"example.com/simple-project/foo"
 	"example.com/simple-project/internal"
+	"time"
 )
 
 func main() {
 	foo.Bar1()
 	foo.Bar2()
 	internal.Hello()
-	select {}
+	time.Sleep(time.Second)
 }
